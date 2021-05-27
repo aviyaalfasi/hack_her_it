9d95c560844b6a964867c89a780e7d253785e4d9
RuthMil <ruth.miller@vatbox.com>
2021-05-27 16:16:54 +0300

    v1

--- argonomic/height_change.py
+++ argonomic/height_change.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 from plyer import notification # for getting notification on your PC
+from argonomic import vidoe_camera
 
 
 def popup_message(accept_title, accept_message):
@@ -11,7 +12,7 @@ def popup_message(accept_title, accept_message):
             message=accept_message,
             # creating icon for the notification
             # we need to download a icon of ico file format
-            app_icon=None,
+            app_icon=r'logo\note1.ico',
 
             # the notification stays for 50sec
             timeout=50
@@ -32,72 +33,92 @@ def get_face_sizes(picture):
         minSize=(30, 30)
     )
     max_face_size = 0
-    max_sizes = {'max_face_size': max_face_size, 'x': 0, 'y': 0, 'w': 0, 'h': 0}
-    for (x, y, w, h) in faces:
-        face_size = h
+    max_sizes = {'max_face_size': max_face_size, 'x': 0, 'y': 0, 'width': 0, 'height': 0}
+    for (x, y, width, height) in faces:
+        face_size = width * height
         if face_size > max_sizes['max_face_size']:
             max_sizes['max_face_size'] = face_size
             max_sizes['x'] = x
             max_sizes['y'] = y
-            max_sizes['w'] = w
-            max_sizes['h'] = h
+            max_sizes['width'] = width
+            max_sizes['height'] = height
 
     return max_sizes
 
 
-def take_picture():
+def take_picture(cap, click_required=False):
     # Open the device at the ID 0
     # Use the camera ID based on
     # /dev/videoID needed
-    cap = cv2.VideoCapture(0)
-
-    # Check if camera was opened correctly
-    if not (cap.isOpened()):
-        print("Could not open video device")
-
-    # Set the resolution
 
     ret, frame = cap.read()
-    cap.release()
+    frame = cv2.resize(frame, None, fx=1, fy=1, interpolation=cv2.INTER_AREA)
+    print("Taking picture...")
+
+    # if needed, wait for click from user
+    if click_required:
+        cv2.imshow("AppRight", frame)
+        keyboard_click = -1
+        while keyboard_click not in [ord('a'),10]:
+            keyboard_click = cv2.waitKey(2)
+            # max_face = get_face_sizes(frame)
+            # cv2.rectangle(frame, (max_face['x'], max_face['y']), (
+            #     max_face['x'] + max_face['width'],
+            #     max_face['y'] + max_face['height']), (0, 255, 0), 2)
+            # cv2.imshow("AppRight", frame)
     return frame
 
 
-def run():
+def restart_app(threshold=30, seconds_to_sleep=3, max_differences=2):
     count_difference = 0
-    TRESHHOLD = 10
-    MAX_DIFFERENCES_AMOUNT = 2
-    SECONDS_TO_SLEEP = 2
-
     picture_number = 0
 
-    initial_picture = take_picture()
+    # waiting for user clicking
+
+    # initial_picture = take_picture(click_required=True)
+    initial_picture = vidoe_camera.show_video()
+    print("********  You are all set!  ********")
     # cv2.imshow("preview", initial_picture)
     initial_picture_face_size = get_face_sizes(initial_picture)
     cv2.rectangle(initial_picture, (initial_picture_face_size['x'], initial_picture_face_size['y']), (
-    initial_picture_face_size['x'] + initial_picture_face_size['w'],
-    initial_picture_face_size['y'] + initial_picture_face_size['h']), (0, 255, 0), 2)
+    initial_picture_face_size['x'] + initial_picture_face_size['width'],
+    initial_picture_face_size['y'] + initial_picture_face_size['height']), (0, 255, 0), 2)
     cv2.imwrite('height_test\initial_picture.jpg', initial_picture)
+
+    cap = cv2.VideoCapture(0)
+    # Check if camera was opened correctly
+    if not (cap.isOpened()):
+        print("Could not open video device")
+        exit()
+    print("Camera opened successfully")
 
     while True:
         picture_number += 1
-        current_picture = take_picture()
+        current_picture = take_picture(cap)
         current_picture_face_size = get_face_sizes(current_picture)
-        cv2.rectangle(current_picture, (current_picture_face_size['x'], current_picture_face_size['y']), (current_picture_face_size['x'] + current_picture_face_size['w'], current_picture_face_size['y'] + current_picture_face_size['h']), (0, 255, 0), 2)
+        cv2.rectangle(current_picture, (current_picture_face_size['x'], current_picture_face_size['y']), (current_picture_face_size['x'] + current_picture_face_size['width'], current_picture_face_size['y'] + current_picture_face_size['height']), (0, 255, 0), 2)
         cv2.imwrite('height_test\\' + str(picture_number) + '.jpg', current_picture)
 
-        if abs(current_picture_face_size['max_face_size'] - initial_picture_face_size['max_face_size']) > TRESHHOLD:
+        if current_picture_face_size['height'] - initial_picture_face_size['height'] > threshold:
             count_difference += 1
         else:
             count_difference = 0
 
-        if count_difference >= MAX_DIFFERENCES_AMOUNT:
-            print('Problem')
+        if count_difference >= max_differences:
+            print('********  Problem!!!  ********')
+            popup_message("AppRight", "Reminder:\nSit healthy")
+            # return False
             count_difference = 0
         # take a picture every 5 seconds
-        time.sleep(SECONDS_TO_SLEEP)
+        time.sleep(seconds_to_sleep)
 
+    cap.release()
+    cv2.destroyAllWindows()
 
-run()
+#
+# def run():
+#     is_stable = restart_app()
+#     while True:
+#         restart_app()
 
-
-
+restart_app()
---
+++ argonomic/vidoe_camera.py
@@ -0,0 +1,47 @@
+# only live feed showing , capture Image (c), start video recording (v), pause video recording (p)
+# resume video recording (r), stop video recording (b), exit program (q)
+# to stop recording and exit (q)
+import cv2
+# from datetime import datetime
+
+
+def start_process():
+	cap = cv2.VideoCapture(0)
+	cv2.namedWindow('AppRight', cv2.WINDOW_NORMAL)
+	return cap
+
+
+def show_video():
+	cap = start_process()
+	global recordControl
+	while True:
+		# now = datetime.now()
+		# time = datetime.time(now)
+		# name = "capture_" + now.strftime("%y%m%d") + time.strftime("%H%M%S") + ".jpg"
+		ret, frame = cap.read()
+		if ret is True:
+			frame = cv2.flip(frame, 1)   # 1 = vertical , 0 = horizontal
+
+			cv2.imshow('AppRight', frame)
+
+			k = cv2.waitKey(1) & 0Xff
+			if k == ord('q'):  # Quit program and recording
+				cap.release()
+				cv2.destroyAllWindows()
+				break
+			elif k in [ord('c'), 32]:  # capture Image
+				cap.release()
+				cv2.destroyAllWindows()
+				return frame
+		else:
+			break
+
+#
+# if (cap.isOpened()):
+# 	show_video()
+# else:
+# 	cap.open()
+# 	show_video()
+#
+# cap.release()
+# cv2.destroyAllWindows()
